0e046131c5c6a9e133722840f7c1ff3c29a9db58
季楠 <2759278070@qq.com>
2024-11-24 10:10:09 +0800

    [1.0.0] [+]Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ taboolib {
         }
     }
     version { taboolib = "6.2.0-beta36" }
-    relocate("ink.ptms.um", "com.github.cpjinan.plugin.akariartifact.um")
+    relocate("ink.ptms.um", "com.github.cpjinan.plugin.akariitem.um")
 }
 
 repositories {
