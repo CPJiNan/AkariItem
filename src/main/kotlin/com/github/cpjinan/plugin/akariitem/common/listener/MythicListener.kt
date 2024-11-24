27159682a396e5be43074bfc6c1fa2ed80fd14f3
季楠 <2759278070@qq.com>
2024-11-24 23:42:46 +0800

    [1.0.0] [+]Initial commit

--- src/main/kotlin/com/github/cpjinan/plugin/akariitem/common/listener/MythicListener.kt
+++ src/main/kotlin/com/github/cpjinan/plugin/akariitem/common/listener/MythicListener.kt
@@ -3,7 +3,6 @@ package com.github.cpjinan.plugin.akariitem.common.listener
 import com.github.cpjinan.plugin.akariitem.api.ItemAPI
 import ink.ptms.um.event.MobDropLoadEvent
 import taboolib.common.platform.event.SubscribeEvent
-import taboolib.platform.util.buildItem
 
 object MythicListener {
     @SubscribeEvent
@@ -11,9 +10,7 @@ object MythicListener {
         event.dropName.split("@", limit = 2).takeIf { it.size == 2 }?.let {
             if (it[0].equals("akariitem", ignoreCase = true)) {
                 ItemAPI.getItem(it[1])?.let { item ->
-                    event.registerItem { dropMeta ->
-                        buildItem(item) { amount = dropMeta.amount.toInt() }
-                    }
+                    event.registerItem { item }
                 }
             }
         }
